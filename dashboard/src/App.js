5f73f1e02aed2c3cb7242ddc6036aabdf332bfb0
Yosef-ft <yosefetene02@gmail.com>
2024-11-06 12:16:48 +0300

    dashbord: line plot of oil price

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './App.css';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  const [priceData, setPriceData] = useState([]);
 
   useEffect(() => {
-    fetch('/test').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
+    fetch('/api/prices')
+      .then(res => res.json())
+      .then(data => {
+        setPriceData(data);
+      });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
+        <h1>Brent Oil Price Data</h1>
+        <ResponsiveContainer width="90%" height={400}>
+          <LineChart data={priceData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="Date" angle={-90} dx={-10} />
+            <YAxis dataKey="Price" />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="Price" stroke="#8884d8" activeDot={{ r: 8 }} />
+          </LineChart>
+        </ResponsiveContainer>
       </header>
     </div>
   );
