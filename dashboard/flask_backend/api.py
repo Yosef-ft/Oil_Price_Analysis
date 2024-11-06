19b0409878bb74a26ddd4dad49f913a4a0a48737
Yosef-ft <yosefetene02@gmail.com>
2024-11-06 12:16:08 +0300

    Updated the api endpoint

--- dashboard/flask_backend/api.py
+++ dashboard/flask_backend/api.py
@@ -1,8 +1,9 @@
-import time
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/test')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/api/prices')
+def get_prices():
+    data = pd.read_csv('data/BrentOilPrices.csv')
+    return jsonify(data.to_dict(orient='records'))
