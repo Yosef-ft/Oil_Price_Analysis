eaac3c12b1799b9b5e7e2cfddd2c0045baed0623
Yosef-ft <yosefetene02@gmail.com>
2024-11-20 07:43:43 +0300

    Added an endpoint for showing model Metrics

--- dashboard/flask_backend/api.py
+++ dashboard/flask_backend/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import pandas as pd
+import json
 
 app = Flask(__name__)
 
@@ -7,3 +8,10 @@ app = Flask(__name__)
 def get_prices():
     data = pd.read_csv('data/BrentOilPrices.csv')
     return jsonify(data.to_dict(orient='records'))
+
+@app.route('/api/metrics')
+def get_model_metrics():
+    
+    with open('data/metrics.json', 'r') as file:
+        metrics = json.load(file)
+    return metrics
